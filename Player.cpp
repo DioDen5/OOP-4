5171f48c4031826bb1a8f27b01761b66294bc776
DenZastavnyi <denyszastavniy@gmail.com>
2025-05-20 12:12:00 +0300

    first commit

---
+++ Player.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Denys Zastavnyi on 20.05.2025.
+//
+
+#include "Player.h"
---
+++ Player.h
@@ -0,0 +1,16 @@
+//
+// Created by Denys Zastavnyi on 20.05.2025.
+//
+
+#ifndef PLAYER_H
+#define PLAYER_H
+
+
+
+class Player {
+
+};
+
+
+
+#endif //PLAYER_H
